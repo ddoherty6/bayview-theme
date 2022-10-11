8ef726800d5a04ff240eb89ee2cbad8508e45d27
Dan Doherty <ddoherty6@gmail.com>
2022-10-11 10:05:52 -0700

    experimenting with wp php library

--- header.php
+++ header.php
@@ -60,23 +60,23 @@
 					
 					<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="Primary Menu">
                         <?php
-                            // if ( has_nav_menu( 'primary' ) ) :
-                            //     wp_nav_menu( [
-                            //         'theme_location' => 'primary',
-                            //         'container'      => false,
-                            //         'menu_class'     => '',
-                            //         'menu_id'        => '',
-                            //         'depth'          => 3
-                            //     ] );
-                            // else :
-                            //     printf(
-                            //         '<a href="%1$s">%2$s</a>',
-                            //         esc_url( admin_url( '/nav-menus.php' ) ),
-                            //         esc_html__( 'Asign a menu', 'herobiz' )
-                            //     );
-                            // endif;
+                            if ( has_nav_menu( 'primary' ) ) :
+                                wp_nav_menu( [
+                                    'theme_location' => 'primary',
+                                    'container'      => false,
+                                    'menu_class'     => '',
+                                    'menu_id'        => '',
+                                    'depth'          => 3
+                                ] );
+                            else :
+                                printf(
+                                    '<a href="%1$s">%2$s</a>',
+                                    esc_url( admin_url( '/nav-menus.php' ) ),
+                                    esc_html__( 'Asign a menu', 'herobiz' )
+                                );
+                            endif;
                         ?>
-						<ul id="primary-menu" class="menu">
+						<!-- <ul id="primary-menu" class="menu">
 							<li id="menu-item-3415" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3415"><a href="./index2.html">Blog</a></li>
 							<li id="menu-item-3444" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3444"><a href="./index2.html">Events</a></li>
 							<li id="menu-item-3403" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3403"><a href="./index2.html">Donate</a></li>
@@ -86,30 +86,30 @@
 							</li>
 							<li id="menu-item-3387" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3387">
 								<a href="./index2.html">Programs<svg class="icon icon-expand" aria-hidden="true" role="img"> <use xlink:href="./wp-content/themes/harrison/assets/icons/genericons-neue.svg#expand"></use> </svg></a>
-							</li>
+							</li> -->
 							<!-- <li id="menu-item-3406" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-3406">
 								<a href="./housing-counseling/index.html">Housing<svg class="icon icon-expand" aria-hidden="true" role="img"> <use xlink:href="./wp-content/themes/harrison/assets/icons/genericons-neue.svg#expand"></use> </svg></a>
 							</li> -->
 							<!-- <li id="menu-item-3416" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3416"><a href="./art-for-aid/index.html">Art for Aid</a></li> -->
-							<li id="menu-item-3402" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3402"><a href="./index2.html">Watch Now</a></li>
-						</ul>
+							<!-- <li id="menu-item-3402" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3402"><a href="./index2.html">Watch Now</a></li> -->
+						<!-- </ul> -->
 					</nav>
                     <?php
-                        // if ( has_nav_menu( 'header_action' ) ) :
-                        //     wp_nav_menu( [
-                        //         'theme_location' => 'header_action',
-                        //         'container'      => false,
-                        //         'menu_class'     => 'header_menu_action',
-                        //         'menu_id'        => '',
-                        //         'depth'          => 3
-                        //     ] );
-                        // else :
-                        //     printf(
-                        //         '<a class="btn-getstarted scrollto" href="%1$s">%2$s</a>',
-                        //         esc_url( admin_url( '/nav-menus.php' ) ),
-                        //         esc_html__( 'Action Menu', 'herobiz' )
-                        //     );
-                        // endif;
+                        if ( has_nav_menu( 'header_action' ) ) :
+                            wp_nav_menu( [
+                                'theme_location' => 'header_action',
+                                'container'      => false,
+                                'menu_class'     => 'header_menu_action',
+                                'menu_id'        => '',
+                                'depth'          => 3
+                            ] );
+                        else :
+                            printf(
+                                '<a class="btn-getstarted scrollto" href="%1$s">%2$s</a>',
+                                esc_url( admin_url( '/nav-menus.php' ) ),
+                                esc_html__( 'Action Menu', 'herobiz' )
+                            );
+                        endif;
                     ?>
 				</div>
 			</div>
